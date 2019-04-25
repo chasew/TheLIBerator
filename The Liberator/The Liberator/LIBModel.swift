2223b55127a804ec0717440ce6724ce052e72052
Allison Wei <weiallison1999@gmail.com>
2019-04-25 17:08:16 -0400

    dude, it's BROKE

--- The Liberator/The Liberator/LIBModel.swift
+++ The Liberator/The Liberator/LIBModel.swift
@@ -24,8 +24,9 @@ class madlib {
     //haha, crying
     init(fileName : String){
         
+        var lines = [String]() //this is literally to extract the title
         words = [String]()
-        theWholeAssString = "that's it that the project"
+        theWholeAssString = "that's it that's the project"
         blanks = [Int:(Bool,String,String)]()
         blankTypes = [String]()
         
@@ -33,14 +34,14 @@ class madlib {
             if let path = Bundle.main.path(forResource: fileName, ofType: "txt"){
                 theWholeAssString = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
                 words = theWholeAssString.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
-//              print(words)
+                lines = theWholeAssString.components(separatedBy: NSCharacterSet.newlines)
             }
         } catch _ as NSError {
             print("F")
         }
         
         do {
-            let regex = try NSRegularExpression(pattern: "[A-Z]+")
+            let regex = try NSRegularExpression(pattern: "<[A-Z ]+> ")
             let nsString = theWholeAssString as NSString
             let results = regex.matches(in: theWholeAssString, range: NSRange(location: 0, length: nsString.length))
             blankTypes = results.map { nsString.substring(with: $0.range)}
@@ -48,17 +49,54 @@ class madlib {
             print("F, part 2")
         }
         
-        for i in 0...blankTypes.count {
-            blanks[i] = (false, blankTypes[i],"")
+        for i in 0..<blankTypes.count {
+            let blankTypeDisplay = blankTypes[i].lowercased()
+            var actualBlankDisplay = "something went wrong, lol"
+            do {
+                let regexBCTHERESNOSUBSTRING = try NSRegularExpression(pattern: "[a-z ]+")
+                let nsString = blankTypeDisplay as NSString
+                let mysteryObject = regexBCTHERESNOSUBSTRING.firstMatch(in: blankTypeDisplay, range: NSRange(location: 0, length: nsString.length))
+                actualBlankDisplay = nsString.substring(with: mysteryObject!.range)
+            } catch {
+                print("F, part 3")
+            }
+            blanks[i] = (false, actualBlankDisplay,"")
         }
         
-        title = words[0]
-        completed = false 
+        title = lines[0]
+        completed = false
+        print(title)
         print(blanks)
-        
+    }
+    
+    func fillBlank(position : Int, text : String){
+        blanks[position]?.0 = true
+        blanks[position]?.2 = text
+    }
+    
+    func deleteBlank(position : Int, text : String){
+        blanks[position]?.0 = false
+    }
+    
+    func getTypeOfPosition(pos : Int) -> String {
+        return blanks[pos]?.1 ?? "something went wrong"
+    }
+    
+    func isComplete() -> Bool {
+        for i in 0..<blanks.count{
+            if(blanks[i]!.0 == false){
+                return false
+            }
+        }
+        return true
+    }
+    
+    func getFullText() -> String{
+        return "lol, do it"
     }
 }
 
 class LIBModel {
     let catagories = [String:[madlib]]()
 }
+
