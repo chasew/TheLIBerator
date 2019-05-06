63dd73b1ba6cf632736f1aef5ba05555aedcc444
Tsipora Stone <tsiporastone@Tsiporas-MacBook-Pro.local>
2019-05-06 00:51:26 -0400

    added an alert if not all blanks are filled

--- The Liberator/The Liberator/FinishedLibViewController.swift
+++ The Liberator/The Liberator/FinishedLibViewController.swift
@@ -13,6 +13,7 @@ class FinishedLibViewController: UIViewController {
     var text = String()
     
     @IBOutlet weak var textView: UITextView!
+     var lib : madlib?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,21 @@ class FinishedLibViewController: UIViewController {
         textView.adjustsFontForContentSizeCategory = true
         textView.center = self.view.center
         textView.textAlignment = NSTextAlignment.justified
+        
+        let indices = (lib?.getFilledIndices())!
+        var words = text.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
+        let attributedString = NSMutableAttributedString.init(string: text)
+        for index in indices {
+            let range = (text as NSString).range(of: words[index - 1])
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.orange, range: range)
+            attributedString.addAttribute(NSAttributedString.Key.font, value: UIFont.boldSystemFont(ofSize: 15), range: range)
+            print(range)
+            print(words[index])
+            textView.attributedText = attributedString
+            textView.isUserInteractionEnabled = false
+            textView.isEditable = false
+        }
+        
     }
     
 
